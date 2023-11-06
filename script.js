4e383e8a8d7b92f3f68b09c75885c4aa19b147b3
Ola-Reda <ola.reda.900@gmail.com>
2023-11-06 02:07:43 +0200

    Add final changes

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const playerXBtn = document.querySelector(".playerX");
 const playerOBtn = document.querySelector(".playerO");
 const main = document.querySelector("main");
 const player = document.querySelector(".player");
-const boxs = document.querySelectorAll(".box");
+const boxs = document.querySelectorAll(".area div");
 const result = document.querySelector(".result");
+const resultText = document.querySelector(".result-text");
 const replayBtn = document.querySelector(".replay");
 
 playerXBtn.addEventListener("click", startGame)
@@ -18,6 +19,7 @@ function startGame() {
 playerOBtn.addEventListener("click", choosePlayer)
 function choosePlayer() {
     player.setAttribute("class", "player active")
+    // player.classList.add("active")
 }
 
 //add function to every button
@@ -28,21 +30,85 @@ boxs.forEach(box => {
 
 let playerXIcon = 'X'
 let playerOIcon = 'O'
-
+let playerSign = 'X'
 
 function clickedBox(element) {
-    console.log(element)
     if(player.classList.contains("active")) {
-        console.log("ooo")
-        element.innerHTML = `${playerOIcon}`
+        playerSign = 'O'
+        element.innerHTML = `<span>${playerOIcon}</span>`
         player.classList.remove("active")
+        element.setAttribute("id", playerSign)
     } else {
-        element.innerHTML = `${playerXIcon}`
+        playerSign = 'X'
+        element.innerHTML = `<span>${playerXIcon}</span>`
         player.classList.add("active")
+        element.setAttribute("id", playerSign)
     }
+    selectWinner()
     element.style.pointerEvents = "none"
+    setTimeout(()=> {
+        bot()
+    }, 900)
 }
 
+
+//function bot
+function bot() {
+    let array = [] 
+    boxs.forEach((box, index) => {
+        if(box.childElementCount == 0) {
+            array.push(index)
+        }
+    })
+    let randomBox = array[Math.floor(Math.random() *array.length)]
+    if(array.length > 0) {
+        if(player.classList.contains('active')) {
+            playerSign = 'O'
+            boxs[randomBox].innerHTML = `<span>${playerOIcon}</span>`
+            player.classList.remove("active")
+            boxs[randomBox].setAttribute("id", playerSign)
+        } else {
+            boxs[randomBox].innerHTML = `<span>${playerXIcon}</span>`
+            player.classList.add("active")
+            playerSign = 'X'
+            boxs[randomBox].setAttribute("id", playerSign)
+        }
+    }
+    selectWinner()
+    boxs[randomBox].style.pointerEvents = "none"
+}
+
+function getId(id) {
+    return document.querySelector('.box' + id).id
+}
+
+function checkWinner(val1, val2, val3, sign) {
+    if(getId(val1) == sign && getId(val2) == sign && getId(val3) == sign) {
+        return true
+    }
+}
+
+//select winner
+function selectWinner() {
+    if(checkWinner(1, 2, 3, playerSign) || checkWinner(4, 5, 6, playerSign) || checkWinner(7, 8, 9, playerSign) || checkWinner(1, 4, 7, playerSign) || 
+    checkWinner(2,5, 8, playerSign) ||  checkWinner(3, 6, 9, playerSign) ||  checkWinner(3, 5, 7, playerSign) ||  checkWinner(1, 5, 9, playerSign)){
+        setTimeout(() => {
+            main.classList.remove("show")
+            result.classList.add("show")
+        }, 800)
+        resultText.innerHTML = `Player ${playerSign} won the game!`
+
+    } else {
+        if (getId(1) != "" && getId(2) != "" && getId(3) != "" && getId(4) != "" && getId(5) != "" && getId(6) != "" && getId(7) != "" && getId(8) != "" && getId(9) != "" ) {
+            setTimeout(() => {
+                main.classList.remove("show")
+                result.classList.add("show")
+            }, 800)
+            resultText.innerHTML = `Match has been drawn!`
+        }
+    }
+    
+}
 
 
 //replay function
